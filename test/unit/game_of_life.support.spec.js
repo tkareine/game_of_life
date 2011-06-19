a970aad03a6e0730e08caf98982caf4fe5eb5e29
Tuomas Kareinen <tkareine@gmail.com>
2011-06-19 05:29:42 +0300

    Add unit tests for canvas validation

--- test/unit/game_of_life.support.spec.js
+++ test/unit/game_of_life.support.spec.js
@@ -1,11 +1,15 @@
 describe('Support module', function () {
   var S = GameOfLife.Support;
 
-  describe('For parsing canvas', function () {
-    it('throws InvalidArgument if given other than canvas element', function () {
+  describe('For validating canvas', function () {
+    it('throws InvalidArgument if given other than a canvas element', function () {
       expect(function () {
-        S.parseCanvas($('<p>foo</p>'));
+        S.validateCanvas($('<p>foo</p>')[0]);
       }).toThrow(new S.InvalidArgument('Not a canvas element'));
+    });
+
+    it('returns the given element if the element is a canvas', function () {
+      expect(S.validateCanvas($('<canvas></canvas>')[0]).getContext).toBeFunction();
     });
   });
 });
