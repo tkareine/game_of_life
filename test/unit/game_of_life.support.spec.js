13348f63a46b9cfc2265bfcf11af9c5e6d3c4fec
Tuomas Kareinen <tkareine@gmail.com>
2011-06-19 18:55:12 +0300

    Add unit tests for cell grid parsing

--- test/unit/game_of_life.support.spec.js
+++ test/unit/game_of_life.support.spec.js
@@ -1,5 +1,6 @@
 describe('Support module', function () {
-  var S = GameOfLife.Support;
+  var S   = GameOfLife.Support
+    , CT  = S.CellTypes;
 
   it('inverts an object', function () {
     var obj = { foo: 1, bar: 2};
@@ -15,6 +16,31 @@ describe('Support module', function () {
 
     it('returns the given element if the element is a canvas', function () {
       expect(S.validateCanvas($('<canvas></canvas>')[0]).getContext).toBeFunction();
+    });
+  });
+
+  describe('For parsing a cell grid', function () {
+    it('throws InvalidArgument if layout is a blank string', function () {
+      expect(function () {
+        S.parseCellGrid(' ');
+      }).toThrow(new S.InvalidArgument('Cell grid is not a two-dimensional matrix'));
+    });
+
+    it('throws InvalidArgument if layout has unknown symbol', function () {
+      var symbol = ','
+      expect(function () {
+        S.parseCellGrid(symbol);
+      }).toThrow(new S.InvalidArgument('Unknown cell symbol: ' + symbol));
+    });
+
+    it('throws InvalidArgument if layout is not a rectangle', function () {
+      expect(function () {
+        S.parseCellGrid("..\n.");
+      }).toThrow(new S.InvalidArgument('Cell grid is not a rectangle'));
+    });
+
+    it('parses a valid layout', function () {
+      expect(S.parseCellGrid("..\n.*")).toEqual([[CT.Dead, CT.Dead], [CT.Dead, CT.Alive]]);
     });
   });
 });
