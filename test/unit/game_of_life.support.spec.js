eb5e847adf69f0d183f6a13f72eb72ce7a1d2dc9
Tuomas Kareinen <tkareine@gmail.com>
2011-06-19 05:52:40 +0300

    Add unit test for object inversion

--- test/unit/game_of_life.support.spec.js
+++ test/unit/game_of_life.support.spec.js
@@ -12,4 +12,9 @@ describe('Support module', function () {
       expect(S.validateCanvas($('<canvas></canvas>')[0]).getContext).toBeFunction();
     });
   });
+
+  it('inverts an object', function () {
+    var obj = { foo: 1, bar: 2};
+    expect(S.invertObject(obj)).toEqual({1: 'foo', 2: 'bar'});
+  });
 });
