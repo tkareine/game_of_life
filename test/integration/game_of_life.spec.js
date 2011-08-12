7106c72420e27bc0dea1f362ed1882a6e213434f
Tuomas Kareinen <tkareine@gmail.com>
2011-08-13 00:45:10 +0300

    Revert "Make placeholders for Kata exercises"
    This reverts commit 4e2940364bf6f61aca0c54bdd33b4db6ead90448.

--- test/integration/game_of_life.spec.js
+++ test/integration/game_of_life.spec.js
@@ -1,10 +1,36 @@
 describe('Game of Life', function () {
+  var GoF = GameOfLife,
+      canvas = $('<canvas></canvas>'),
+      seed =
+        "......\n" +
+        "...*..\n" +
+        ".***..\n" +
+        "......\n" +
+        "......\n",
+      expectedHaltGeneration = GoF.Support.parseCellGrid(
+        "......\n" +
+        "..**..\n" +
+        ".*..*.\n" +
+        "..**..\n" +
+        "......\n"),
+      expectedNumGenerations = 3,
+      gfxDrawFrameSpy,
+      gof;
+
   beforeEach(function () {
-    // TODO: Prepare a spy for the graphics module for checking the result of
-    // `gof.evolve()`.
+    // test the outcome via a spy version of Graphics object
+    gfxDrawFrameSpy = jasmine.createSpy();
+    spyOn(GoF, 'Graphics').andReturn({ drawFrame: gfxDrawFrameSpy });
+
+    // call timeouts immediately so we get the results
+    spyOn(window, 'setTimeout').andCallFake(function (callback, timeout) { callback(); });
+
+    gof = GoF(seed, canvas[0]);
   });
 
   it('evolves generations until no change in the current generation', function () {
-    // TODO: Write more tests
+    gof.evolve();
+    expect(gfxDrawFrameSpy.mostRecentCall.args).toEqual([expectedHaltGeneration, expectedNumGenerations]);
+    expect(gfxDrawFrameSpy.callCount).toEqual(expectedNumGenerations + 1);
   });
 });
