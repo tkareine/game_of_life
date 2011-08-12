9624eed21e2635a4eb1d18d299dcfb80d2e91671
Tuomas Kareinen <tkareine@gmail.com>
2011-08-13 01:28:02 +0300

    Raise evolveCell to be a public function

--- test/unit/game_of_life.generation.spec.js
+++ test/unit/game_of_life.generation.spec.js
@@ -41,6 +41,30 @@ describe('Generation module', function () {
         ]
       };
 
+  describe('For evolving a cell', function () {
+    it('evolves alive cell to alive if the cell has 2 or 3 neighbors', function () {
+      expect(G.evolveCell(CT.Alive, 2)).toEqual(CT.Alive);
+      expect(G.evolveCell(CT.Alive, 3)).toEqual(CT.Alive);
+    });
+
+    it('evolves alive cell to dead if the cell has less than 2 neighbors', function () {
+      expect(G.evolveCell(CT.Alive, 1)).toEqual(CT.Dead);
+    });
+
+    it('evolves alive cell to dead if the cell has more than 3 neighbors', function () {
+      expect(G.evolveCell(CT.Alive, 4)).toEqual(CT.Dead);
+    });
+
+    it('evolves dead cell to alive if the cell has 3 neighbors', function () {
+      expect(G.evolveCell(CT.Dead, 3)).toEqual(CT.Alive);
+    });
+
+    it('evolves dead cell to dead if the cell has not 3 neighbors', function () {
+      expect(G.evolveCell(CT.Dead, 2)).toEqual(CT.Dead);
+      expect(G.evolveCell(CT.Dead, 4)).toEqual(CT.Dead);
+    });
+  });
+
   describe('For evolvining the next generation (tick)', function () {
     it('evolves all dead cells to dead cells', function () {
       expect(G.tick(Generations.allDead)).toEqual(Generations.allDead);
