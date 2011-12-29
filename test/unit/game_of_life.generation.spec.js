f05ae81d661e967e3033f26e591c2070740bc40f
Tuomas Kareinen <tkareine@gmail.com>
2011-12-29 21:04:27 +0200

    More visual (and thus readable) generation spec

--- test/unit/game_of_life.generation.spec.js
+++ test/unit/game_of_life.generation.spec.js
@@ -3,41 +3,47 @@ describe('Generation module', function () {
       CT          = GameOfLife.Support.CellTypes,
       Generations = {
         allDead: [
-          [ CT.Dead,  CT.Dead,  CT.Dead  ],
-          [ CT.Dead,  CT.Dead,  CT.Dead  ],
-          [ CT.Dead,  CT.Dead,  CT.Dead  ]
+          [ '.', '.', '.' ],
+          [ '.', '.', '.' ],
+          [ '.', '.', '.' ]
         ],
         singleAlive: [
-          [ CT.Dead,  CT.Dead,  CT.Dead  ],
-          [ CT.Dead,  CT.Alive, CT.Dead  ],
-          [ CT.Dead,  CT.Dead,  CT.Dead  ]
+          [ '.', '.', '.' ],
+          [ '.', '*', '.' ],
+          [ '.', '.', '.' ]
         ],
         blockStillLive: [
-          [ CT.Dead,  CT.Dead,  CT.Dead  ],
-          [ CT.Dead,  CT.Alive, CT.Alive ],
-          [ CT.Dead,  CT.Alive, CT.Alive ]
+          [ '.', '.', '.' ],
+          [ '.', '*', '*' ],
+          [ '.', '*', '*' ]
         ],
         blinkerOscillatorHorizontal: [
-          [ CT.Dead,  CT.Dead,  CT.Dead  ],
-          [ CT.Alive, CT.Alive, CT.Alive ],
-          [ CT.Dead,  CT.Dead,  CT.Dead  ]
+          [ '.', '.', '.', '.', '.' ],
+          [ '.', '.', '.', '.', '.' ],
+          [ '.', '*', '*', '*', '.' ],
+          [ '.', '.', '.', '.', '.' ],
+          [ '.', '.', '.', '.', '.' ]
         ],
         blinkerOscillatorVertical: [
-          [ CT.Dead,  CT.Alive, CT.Dead  ],
-          [ CT.Dead,  CT.Alive, CT.Dead  ],
-          [ CT.Dead,  CT.Alive, CT.Dead  ]
+          [ '.', '.', '.', '.', '.' ],
+          [ '.', '.', '*', '.', '.' ],
+          [ '.', '.', '*', '.', '.' ],
+          [ '.', '.', '*', '.', '.' ],
+          [ '.', '.', '.', '.', '.' ]
         ],
         gliderSpaceshipPhase1: [
-          [ CT.Dead,  CT.Dead,  CT.Alive, CT.Dead  ],
-          [ CT.Alive, CT.Dead,  CT.Alive, CT.Dead  ],
-          [ CT.Dead,  CT.Alive, CT.Alive, CT.Dead  ],
-          [ CT.Dead,  CT.Dead,  CT.Dead,  CT.Dead  ]
+          [ '.', '.', '*', '.', '.' ],
+          [ '*', '.', '*', '.', '.' ],
+          [ '.', '*', '*', '.', '.' ],
+          [ '.', '.', '.', '.', '.' ],
+          [ '.', '.', '.', '.', '.' ]
         ],
         gliderSpaceshipPhase2: [
-          [ CT.Dead,  CT.Alive, CT.Dead,  CT.Dead  ],
-          [ CT.Dead,  CT.Dead,  CT.Alive, CT.Alive ],
-          [ CT.Dead,  CT.Alive, CT.Alive, CT.Dead  ],
-          [ CT.Dead,  CT.Dead,  CT.Dead,  CT.Dead  ]
+          [ '.', '*', '.', '.', '.' ],
+          [ '.', '.', '*', '*', '.' ],
+          [ '.', '*', '*', '.', '.' ],
+          [ '.', '.', '.', '.', '.' ],
+          [ '.', '.', '.', '.', '.' ]
         ]
       };
 
